c45223239098a8b73cf427ced00db3a626d5fdd4
perfection1st <0v3rkillgaming@gmail.com>
2022-10-12 16:03:22 -0500

    Fixed server.js env

--- app/server.js
+++ app/server.js
@@ -1,10 +1,11 @@
 'use strict';
+const API = process.env.TODOAPI || 'http://localhost:3001/api';
 const PORT = process.env.PORT;
 const express = require('express');
 const app = express();
 
 
-console.log(PORT);
+console.log('API:', API, 'PORT:', PORT);
 
 app.use(express.static('app/public'));
 
